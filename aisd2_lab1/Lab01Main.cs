fa2d8fa00a488d32489aa6261e2301806eb6dcd3
Weronika Głuszczak <weronika.gluszczak@wp.pl>
2019-04-10 18:18:50 +0200

    Importing project

---
+++ aisd2_lab1/Lab01Main.cs
@@ -0,0 +1,149 @@
+﻿
+using System;
+
+namespace ASD
+{
+
+public enum SortingAlgorithm { Undefined, QuickSort, ShellSort, HeapSort, MergeSort }
+
+public class SortingTestCase : TestCase
+    {
+
+    private int[] tab;
+    private int[] sortedTab;
+    private SortingAlgorithm algorithm;
+
+    public SortingTestCase(double timeLimit, Exception expectedException, string description, int[] source, int[] expectedResult, SortingAlgorithm algorithm)
+        : base(timeLimit, expectedException, description)
+        {
+        this.tab = (int[])source.Clone();
+        this.sortedTab = (int[])expectedResult.Clone();
+        this.algorithm = algorithm;
+        }
+
+    protected override void PerformTestCase(object prototypeObject)
+        {
+        SortingMethods sortingMethodTester = (SortingMethods)prototypeObject;
+        switch ( algorithm )
+            {
+            case SortingAlgorithm.QuickSort:
+                tab = sortingMethodTester.QuickSort(tab);
+                break;
+            case SortingAlgorithm.ShellSort:
+                tab = sortingMethodTester.ShellSort(tab);
+                break;
+            case SortingAlgorithm.HeapSort:
+                tab = sortingMethodTester.HeapSort(tab);
+                break;
+            case SortingAlgorithm.MergeSort:
+                tab = sortingMethodTester.MergeSort(tab);
+                break;
+            }
+        }
+
+
+    protected override (Result resultCode, string message) VerifyTestCase(object settings)
+        {
+        return System.Linq.Enumerable.SequenceEqual(tab,sortedTab) ?
+               (Result.Success,"OK") : (Result.WrongResult, "Elements haven't been sorted properly");
+        }
+
+    }
+
+class SortingTestModule : TestModule
+    {
+
+    public override void PrepareTestSets()
+        {
+
+        const int testCaseNumber = 9;
+        Random rnd;
+        int[][] tabs = new int[testCaseNumber][];
+        int[][] sortedTabs = new int[testCaseNumber][];
+
+        tabs[0] = new int[2] { 3, -2 };
+        tabs[1] = new int[10] { 5, 8, -3, 10, 2, 5, 7, -6, 5, 1 };
+        tabs[2] = new int[1] { -1 };
+        tabs[3] = new int[0];
+
+        tabs[4] = new int[500];
+        for ( int i=0 ; i<tabs[4].Length ; ++i )
+            tabs[4][i] = i-250;
+
+        rnd = new Random(123);
+        tabs[5] = new int[800];
+        for ( int i=0 ; i<tabs[5].Length ; ++i )
+            tabs[5][i] = rnd.Next(-500,500);
+
+        rnd = new Random(125);
+        tabs[6] = new int[2000];
+        for ( int i=0 ; i<tabs[6].Length ; ++i )
+            tabs[6][i] = rnd.Next();
+
+        rnd = new Random(127);
+        tabs[7] = new int[1000];
+        for ( int i=0 ; i<tabs[7].Length ; ++i )
+            tabs[7][i] = rnd.Next(-10,10);
+
+        rnd = new Random(129);
+        tabs[8] = new int[1600];
+        for ( int i=0 ; i<tabs[8].Length ; ++i )
+            tabs[8][i] = rnd.Next(-1000,1000);
+
+        for ( int k=0 ; k<testCaseNumber ; ++k )
+            {
+            sortedTabs[k] = (int[])tabs[k].Clone();
+            Array.Sort(sortedTabs[k]);
+            }
+
+        double[] limits = new double[testCaseNumber] { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+        string[] descriptions = new string[testCaseNumber]
+            {
+            "bardzo prosty test",
+            "prosty test",
+            "tablica jednoelementowa",
+            "tablica pusta",
+            "tablica posortowana",
+            "dane losowe 1",
+            "dane losowe 2",
+            "dane losowe 3",
+            "dane losowe 4",
+            };
+
+        TestSets["LabQuickSortTests"]    = new TestSet(new SortingMethods(), "Lab. - QuickSort");
+        TestSets["LabShellSortTests"]    = new TestSet(new SortingMethods(), "Lab. - ShellSort");
+        TestSets["LabHeapSortSortTests"] = new TestSet(new SortingMethods(), "Lab. - HeapSort");
+        TestSets["LabMergeSortTests"]    = new TestSet(new SortingMethods(), "Lab. - MergeSort");
+
+        for ( int k=0 ; k<testCaseNumber ; ++k )
+            {
+            TestSets["LabQuickSortTests"].TestCases.Add(new SortingTestCase(limits[k],null,descriptions[k], tabs[k], sortedTabs[k], SortingAlgorithm.QuickSort));
+            TestSets["LabShellSortTests"].TestCases.Add(new SortingTestCase(limits[k],null,descriptions[k], tabs[k], sortedTabs[k], SortingAlgorithm.ShellSort));
+            TestSets["LabHeapSortSortTests"].TestCases.Add(new SortingTestCase(limits[k],null,descriptions[k], tabs[k], sortedTabs[k], SortingAlgorithm.HeapSort));
+            TestSets["LabMergeSortTests"].TestCases.Add(new SortingTestCase(limits[k],null,descriptions[k], tabs[k], sortedTabs[k], SortingAlgorithm.MergeSort));
+            }
+
+        }
+
+    public override double ScoreResult()
+        {
+        return 1;
+        }
+
+    }
+
+class Lab01
+    {
+
+    static void Main(string[] args)
+        {
+        SortingTestModule sortingTests = new SortingTestModule();
+        sortingTests.PrepareTestSets();
+        foreach (var ts in sortingTests.TestSets)
+            ts.Value.PerformTests(false);
+        }
+
+    }
+
+}
---
+++ aisd2_lab1/Sorting.cs
@@ -0,0 +1,221 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ASD
+{
+
+
+
+public class SortingMethods: MarshalByRefObject
+    {
+
+        public int Partition(int l, int r, int[] tab)
+        {
+            int v = tab[l];
+            int i = l;
+            int j = r + 1;
+            do
+            {
+                do i++; while (tab[i] < v && i < r);
+                do j--; while (tab[j] > v);
+                if (i < j)
+                {
+                    int t = tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = t;
+                }
+            } while (i < j);
+            tab[l] = tab[j];
+            tab[j] = v;
+            return j;
+        }
+        public void Quick(int l, int r, int[] tab)
+        {
+            int j = Partition(l, r, tab);
+            if (j - 1 > l) Quick(l, j - 1, tab);
+            if (j + 1 < r) Quick(j + 1, r, tab);
+        }
+        public int[] QuickSort(int[] A)
+        {
+            Random rand = new Random();
+            int length = A.Length;
+            int index, pom;
+            for (int i = 0; i < length; i++)
+            {
+                index = rand.Next(length - 1);
+                pom = A[index];
+                A[index] = A[i];
+                A[i] = pom;
+            }
+            if (A.Length == 1 || A.Length == 0) return A;
+            Quick(0, A.Length - 1, A);
+            return A;
+        }
+
+        public int[] ShellSort(int[] A)
+        {
+            int[] tab = new int[A.Length + 1];
+            for (int i = 0; i < A.Length; i++)
+            {
+                tab[i + 1] = A[i];
+            }
+
+            int h = 2;
+            int n = tab.Length-1;
+            while(h-1<n/2)
+            {
+                h *= 2;
+            }
+            h--;
+            while (h>=1)
+            {
+                for (int j=h+1; j<=n; j++)
+                {
+                    int v = tab[j];
+                    int i = j - h;
+                    while (i>0 && tab[i]>v)
+                    {
+                        tab[i + h] = tab[i];
+                        i -= h;
+                    }
+                    tab[i + h] = v;
+                }
+                h = (h + 1) / 2 - 1;
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = tab[i+1];
+            }
+            return A;
+        }
+
+
+        public int[] CreateHeap(int[] A)
+        {
+            int nn = A.Length-1 ;
+            int n = A.Length-1;
+            for (int i = nn / 2; i >= 1; i--)
+            { A = DownHeap(i, n, A);
+                n = A.Length - 1; }
+            return A;
+        }
+        public int[] DownHeap(int i, int n, int[]A)
+        {
+            int k = 2 * i;
+            int v = A[i];
+            while(k<=n)
+            {
+                if (k + 1 <= n)
+                    if (A[k + 1] > A[k])
+                        k++;
+                if (A[k]>v)
+                {
+                    A[i] = A[k];
+                    i = k;
+                    k = 2 * i;
+                }
+                else
+                    break;
+
+            }
+            A[i] = v;
+            return A;
+        }
+
+    public int[] HeapSort(int[] tab)
+        {
+            if (tab.Length == 1) return tab;
+            int[] A =new int[tab.Length+1];
+            for (int i=0; i<tab.Length; i++)
+            {
+                A[i + 1] = tab[i];
+            }
+         
+            A = CreateHeap(A);
+
+            int hl = A.Length - 1;
+            while (hl>1)
+            {
+                int tmp = A[1];
+                A[1] = A[hl];
+                A[hl] = tmp;
+                hl--;
+                A=DownHeap(1, hl, A);
+            }
+            for (int i = 0; i < tab.Length; i++)
+            {
+               tab[i]= A[i + 1];
+            }
+            return tab;
+        }
+        
+        public void pom(int l, int r, int[] tab)
+        {
+            if (l >= r) return ;
+            int m = (l + r) / 2;
+            if(l<m) pom(l, m, tab);
+            if(m+1<r) pom(m + 1, r, tab);
+            Merge(l, m, m + 1, r, tab);
+    
+        }
+        public void Merge(int l, int m, int n, int r, int[] tab)
+        {
+            int[] tab2 = new int[m-l+r-n+1+1];
+            
+            int i = 0;
+            int poz = 0, pozl=l, pozn=n;
+            while (pozl<=m && pozn<=r)
+            {
+                if (tab[pozl]<=tab[pozn])
+                {
+                    tab2[poz] = tab[pozl];
+                    pozl++;
+                    poz++;
+                }
+                else
+                {
+                    tab2[poz] = tab[pozn];
+                    pozn++;
+                    poz++;
+                }
+               
+            }
+           
+            if (pozl <= m)
+            {
+               
+                for (i = pozl; i <= m; i++)
+                {
+                    tab2[poz] = tab[i];
+                    poz++;
+                }
+            }
+            else if (pozn<=r)
+            {
+                
+                for (i = pozn; i <= r; i++)
+                {
+                    tab2[poz] = tab[i];
+                    poz++;
+                }
+            }
+
+            for (i=l; i<=r;i++)
+            tab[i] = tab2[i-l];
+
+    
+        }
+        public int[] MergeSort(int[] tab)
+        {
+            if (tab.Length == 0 || tab.Length == 1) return tab;
+           
+            pom(0, tab.Length-1, tab);
+      
+            return tab;
+        }
+
+    }
+
+}
